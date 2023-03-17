3ff39b4b3ce7cfead411e52a3818ea21e1f5d2d2
ZİLAN ELEFTOZ <lzilaneleftozl@gmail.com>
2023-03-17 15:29:57 +0300

    Java1-2-week

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,39 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		String ortaMetin ="İlginizi çekebilir";
+		String altMetin="Vade süresi";
+		
+		System.out.println( ortaMetin);
+		
+		int  vade=12;
+		double dolarDun= 18.10;
+		double dolarBugun=18.10;
+		boolean dolarDustuMu=false;	
+		String okYonu = "";
+		
+		if (dolarBugun<dolarDun) {
+			okYonu="down.svg";
+			System.out.println(okYonu);
+			}
+		
+			else if (dolarBugun>dolarDun) {
+				 okYonu="up.svg";
+				 System.out.println(okYonu);
+			}	
+		 else  {
+		 okYonu="equal.svg";
+			System.out.println(okYonu);
+		}
+		
+		String[] krediler= {"Hızlı kredi","maaşınız halkbnatan","mutlu emeklı","yavaş kredi"};	
+	for (int i = 0; i < krediler.length; i++) {
+		System.out.println(krediler[i]);
+	}
+	
+	}
+	
+}
